1c19c1d401a89b4eb98d15daf6ff2a530ace6ef3
EPAY-PRESCO\ian.fan <ian.fan@presco.ws>
2024-07-16 15:31:51 +0800

    條整 config 檔

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? 'vue-firebase' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-firebase/' : '/'
 })
